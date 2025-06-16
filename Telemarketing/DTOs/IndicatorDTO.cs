97bae33ceccfaa38299b69beed7960fb49eda1e3
Marco Giacomini <giacomini_m.a@hotmail.com>
2025-06-16 11:58:08 -0300

    Create calculete test

--- Telemarketing/DTOs/IndicatorDTO.cs
+++ Telemarketing/DTOs/IndicatorDTO.cs
@@ -16,10 +16,20 @@ namespace Telemarketing.DTOs
         {
             if (TypeCalc == TypeCalc.Soma)
             {
+                if (this.Collects == null || this.Collects.Count == 0)
+                {
+                    this.CalcValue = 0.0;
+                    return;
+                }
                 this.CalcValue = this.Collects.Sum(c => c.Value);
             }
             else if (TypeCalc == TypeCalc.Media)
             {
+                if (this.Collects == null || this.Collects.Count == 0)
+                {
+                    this.CalcValue = 0.0;
+                    return;
+                }
                 this.CalcValue = (this.Collects.Count > 0 ? this.Collects.Average(c => c.Value) : 0.0);
             }
         }
